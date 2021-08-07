86d7f590b94ccce9bb56f99a83a59988bf8f554d
Anil Pujari <pujarianil4@gmail.com>
2021-08-08 00:59:10 +0530

    user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true, minLength: 5},
     name: {type: String, required: false},
+    twitch: {type: String, required: false}
     rating:[]
 }, {
     versionKey: false,
