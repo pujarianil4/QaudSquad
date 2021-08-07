7ab48959c4062baf2a4fa0d39480bf1bef979edc
Surya S <77038659+surya-sd@users.noreply.github.com>
2021-08-08 00:31:08 +0530

    styling updated

--- frontend/src/Components/Welcome/Welcome.jsx
+++ frontend/src/Components/Welcome/Welcome.jsx
@@ -1,37 +1,36 @@
-import { Button} from '@material-ui/core';
-import React, { useState } from 'react';
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
 import Modal from "@material-ui/core/Modal";
 import Paper from "@material-ui/core/Paper";
 import styles from "./Welcome.module.css";
 const Welcome = () => {
-const [show,setShow]=useState(false)
-    return (
-        <>
-        <Modal 
-            open={show}
-            onClose={() => setShow(false)}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-        >
-            <Paper>
-                <form action="#">
-                    
-                </form>
-            </Paper>
-
-        </Modal>
-        <div className={styles.container}>
-            <div className={styles.title}>
-
-            <h1>Find Your Perfect Online Class  </h1>
-             <Button onClick={()=>setShow(true)} variant="contained" >Start</Button>
-            </div>
-            <div className={styles.image}>
-                <img src="code.png" alt="" />
-            </div>
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Modal
+        open={show}
+        onClose={() => setShow(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <Paper>
+          <form action="#"></form>
+        </Paper>
+      </Modal>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h1>Find Your Perfect Online Class </h1>
+          <Button onClick={() => setShow(true)} variant="contained">
+            Start
+          </Button>
         </div>
-        </>
-    );
+        <div className={styles.image}>
+          {/* <img src="code.png" alt="" /> */}
+          <img src="Programming1.gif" autoPlay></img>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Welcome;
--- frontend/src/Pages/Signin/Signin.jsx
+++ frontend/src/Pages/Signin/Signin.jsx
@@ -1,94 +1,8 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import {useDispatch} from "react-redux"
-import {SigninApi,SignUpApi} from "../../Redux/AuthReducer/AuthAction"
-import "./Signin.css"
-=======
 import React, { useState } from "react";
 import "./Signin.css";
->>>>>>> 98d247b3c0209ca052302daa36785fb118ba9ea7
 const Signin = () => {
   const [active, setActive] = useState("");
 
-<<<<<<< HEAD
-    const [active,setActive]=useState("")
-	const dispatch= useDispatch()
-	const handleTab=(clas)=>{
-        setActive(clas)
-    }
-	// ----------------------------------Sign In start-------------------------------//
-   const [email1,setEmail1]=useState("")
-   const [password1,setPassword1]=useState("")
-
-	const handleSignIn=(e)=>{
-		console.log("handlesign");
-        e.preventDefault()
-        dispatch(SigninApi({email:email1,password:password1}))
-		
-	}
-	// ----------------------------------Sign In start-------------------------------//
-		// ----------------------------------Sign In start-------------------------------//
-		const [email2,setEmail2]=useState("")
-		const [password2,setPassword2]=useState("")
-	   const [name,setName]=useState("")
-		 const handleSignUp=(e)=>{
-			
-			 e.preventDefault()
-			 dispatch(SignUpApi({email:email2,password:password2,name:name}))
-		 }
-		 // ----------------------------------Sign In start-------------------------------//
-
-    return (
-        <div className="Maincontainer">
-            
-<div className={`container ${active}`} id="container">
-	<div className="form-container right-panel-active sign-up-container">
-		<form action="#">
-			<h1>Create Account</h1>
-			{/* <div className="social-container">
-				<a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-				<a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-				<a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-			</div> */}
-			
-			<input value={name} onChange={(e)=>setName(e.target.value)} type="text" placeholder="Name" />
-			<input value={email2} onChange={(e)=>setEmail2(e.target.value)} type="email" placeholder="Email" />
-			<input value={password2} onChange={(e)=>setPassword2(e.target.value)} type="password" placeholder="Password" />
-			<button onClick={handleSignUp}>Sign Up</button>
-		</form>
-	</div>
-	<div className="form-container sign-in-container">
-		<form action="" >
-			<h1>Sign in</h1>
-			{/* <div className="social-container">
-				<a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-				<a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-				<a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-			</div> */}
-		
-			<input type="email" value={email1} onChange={(e)=>setEmail1(e.target.value)} placeholder="Email" />
-			<input type="password" value={password1} onChange={(e)=>setPassword1(e.target.value)} placeholder="Password" />
-			<a href="#">Forgot your password?</a>
-			<button onClick={handleSignIn}>Sign In</button>
-		</form>
-	</div>
-	<div className="overlay-container">
-		<div className="overlay">
-			<div className="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button className="ghost" id="signIn" onClick={()=>handleTab("")}>Sign In</button>
-			</div>
-			<div className="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button className="ghost" id="signUp" onClick={()=>handleTab(" right-panel-active")}>Sign Up</button>
-			</div>
-		</div>
-	</div>
-</div>
-
-=======
   const handleSignIn = (clas) => {
     setActive(clas);
   };
@@ -119,7 +33,6 @@ const Signin = () => {
 				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div> */}
->>>>>>> 98d247b3c0209ca052302daa36785fb118ba9ea7
 
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
