d7697cbac5ad78f0e1af697bead20d9de4da6cd0
Surya S <77038659+surya-sd@users.noreply.github.com>
2021-08-07 11:27:38 +0530

    videoStream page created and routes added

--- frontend/src/Pages/Routes.jsx
+++ frontend/src/Pages/Routes.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
-import {Route, Switch } from 'react-router-dom'
-import { Home } from './Home'
-import {NavBar } from './NavBar'
-import { Signin } from './Signin'
-import { Signup } from './Signup'
-import Stream from './Stream/Stream'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./Home";
+import { NavBar } from "./NavBar";
+import { Signin } from "./Signin";
+import { Signup } from "./Signup";
+import Stream from "./Stream/Stream";
+import { VideoStream } from "./video/VideoStream";
 
 export const Routes = () => {
-    return (
-        <div className="route">
-            <NavBar />
-            <Switch>
-                <Route exact path="/">    
-                    <Home/>
-                </Route>
-                <Route exact path="/signin">
-                    <Signin/>
-                </Route>
-		        <Route exact path="/signup">
-                   <Signup/>
-                </Route>
-                <Route exact path="/chat">
-                  <Stream/>
-                </Route>
-                <Route>
-                    <h3>
-                        error
-                    </h3>
-                </Route>
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div className="route">
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/signin">
+          <Signin />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/chat">
+          <Stream />
+        </Route>
+        <Route exact path="/videoStream">
+          <VideoStream />
+        </Route>
+        <Route>
+          <h3>error</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+};
